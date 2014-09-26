8177e3a2f04a4e31b2e0939729a67604c2a5c749
Tom Coleman <tom@thesnail.org>
2014-09-26 13:58:47 +1000

    Nice steps

--- async.js
+++ async.js
@@ -1,17 +1,28 @@
 if (Meteor.isServer) {
-  var STEP = 6;
-  
   Meteor.methods({
-    sendEmail: function() {
-      return steps[STEP]('tom@thesnail.org', 'hello tom!');
+    sendEmail: function(step) {
+      if (step !== 1 && step !== 2) {
+        try {
+          steps[step]('tom@thesnail.org', 'hello tom!');
+          console.log('it worked!');
+        } catch (error) {
+          console.log('error', error);
+        }
+      } else {
+        // steps one and two requires a callback
+        steps[step]('tom@thesnail.org', 'hello tom!', function(error) {
+          if (error) {
+            console.log('error', e);
+          } else {
+            console.log('it worked!');
+          }
+        });
+      }
+      
     }
   })
 } else {
-  send = function() {
-    Meteor.call('sendEmail', function(error, response) {
-      if (error) {
-        console.log(error);
-      }
-    })
+  send = function(step) {
+    Meteor.call('sendEmail', step);
   }
 }
